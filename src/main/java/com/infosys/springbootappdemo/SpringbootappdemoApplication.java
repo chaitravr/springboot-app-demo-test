c5f48be8d6f55dbed2137abd7353f351f0189b45
Chaitra Varadaraju <chaitra.varadaraju@ad.infosys.com>
2019-03-28 14:46:38 -0400

    First Commit

---
+++ src/main/java/com/infosys/springbootappdemo/SpringbootappdemoApplication.java
@@ -0,0 +1,13 @@
+package com.infosys.springbootappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootappdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootappdemoApplication.class, args);
+	}
+
+}
